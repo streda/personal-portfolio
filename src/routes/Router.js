e76582eec729f163a03893c1fe61ed747baf498f
streda <samuelreda2018@gmail.com>
2024-06-18 10:44:58 -0700

    Clean case-sensitive files

---
+++ src/routes/Router.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from '../components/Home';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Resume from '../components/Resume';
+
+
+
