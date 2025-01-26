2247eb9e5d7474fc979aea640e0bed8c5275ef74
streda <samuelreda2018@gmail.com>
2025-01-25 19:55:51 -0800

    Deploy

--- test-email.ts
+++ test-email.ts
@@ -1,35 +1,35 @@
-// import nodemailer from 'nodemailer';
-// import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
 
-// // Load environment variables
-// dotenv.config();
+// Load environment variables
+dotenv.config();
 
-// // Configure the transporter
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
+// Configure the transporter
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-// // Configure the email options
-// const mailOptions = {
-//   from: process.env.EMAIL_USER,
-//   to: process.env.EMAIL_RECEIVER,
-//   subject: 'Test Email',
-//   text: 'This is a test email.',
-// };
+// Configure the email options
+const mailOptions = {
+  from: process.env.EMAIL_USER,
+  to: process.env.EMAIL_RECEIVER,
+  subject: 'Test Email',
+  text: 'This is a test email.',
+};
 
-// // Send the email
-// transporter.sendMail(mailOptions, (err, info) => {
-//   if (err) {
-//     console.error('Error:', err); // Print the whole error
-//     if ((err as any).code) {
-//       // Check if the error object has a 'code' property
-//       console.error('Error Code:', (err as any).code);
-//     }
-//     return;
-//   }
-//   console.log('Email sent:', info.response);
-// });
+// Send the email
+transporter.sendMail(mailOptions, (err, info) => {
+  if (err) {
+    console.error('Error:', err); // Print the whole error
+    if ((err as any).code) {
+      // Check if the error object has a 'code' property
+      console.error('Error Code:', (err as any).code);
+    }
+    return;
+  }
+  console.log('Email sent:', info.response);
+});
